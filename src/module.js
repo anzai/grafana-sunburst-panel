5d2a0aa471a31ecb8564031f0d28f8a264e9c917
Nozomi Anzai <anzai@sraoss.co.jp>
2016-06-24 14:21:37 +0900

    Add css

--- src/module.js
+++ src/module.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 import {SunburstCtrl} from './sunburst_ctrl';
 
+/*
+loadPluginCss({
+  dark: 'plugins/grafana-sunburst-panel/css/sunburst.dark.css',
+  light: 'plugins/grafana-sunburst-panel/css/sunburst.light.css'
+});
+*/
+
 export {
   SunburstCtrl as PanelCtrl
 };

5444d93ee73acf1864076898f90b4dc5fe4b558d
Nozomi Anzai <anzai@sraoss.co.jp>
2016-07-04 18:11:35 +0900

    Add light/dark css

--- dist/module.js
+++ dist/module.js
@@ -1,17 +1,25 @@
 'use strict';
 
-System.register(['lodash', './sunburst_ctrl'], function (_export, _context) {
+System.register(['lodash', './sunburst_ctrl', 'app/plugins/sdk'], function (_export, _context) {
   "use strict";
 
-  var _, SunburstCtrl;
+  var _, SunburstCtrl, loadPluginCss;
 
   return {
     setters: [function (_lodash) {
       _ = _lodash.default;
     }, function (_sunburst_ctrl) {
       SunburstCtrl = _sunburst_ctrl.SunburstCtrl;
+    }, function (_appPluginsSdk) {
+      loadPluginCss = _appPluginsSdk.loadPluginCss;
     }],
     execute: function () {
+
+      loadPluginCss({
+        dark: 'plugins/grafana-sunburst-panel/css/sunburst.dark.css',
+        light: 'plugins/grafana-sunburst-panel/css/sunburst.light.css'
+      });
+
       _export('PanelCtrl', SunburstCtrl);
     }
   };
--- src/module.js
+++ src/module.js
@@ -1,12 +1,11 @@
 import _ from 'lodash';
 import {SunburstCtrl} from './sunburst_ctrl';
+import {loadPluginCss} from 'app/plugins/sdk';
 
-/*
 loadPluginCss({
-  dark: 'plugins/grafana-sunburst-panel/css/sunburst.dark.css',
+  dark:  'plugins/grafana-sunburst-panel/css/sunburst.dark.css',
   light: 'plugins/grafana-sunburst-panel/css/sunburst.light.css'
 });
-*/
 
 export {
   SunburstCtrl as PanelCtrl

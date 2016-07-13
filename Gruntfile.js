fd2fa320187deca2e12d783ae2c27cfae5225770
Nozomi Anzai <anzai@sraoss.co.jp>
2016-07-13 13:26:46 +0900

    Update LICENCE, etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
       },
       pluginDef: {
         expand: true,
-        src: ['plugin.json', 'README.md'],
+        src: ['plugin.json', 'README', 'NOTICE'],
         dest: 'dist',
       },
       js_to_dist: {
--- README.md
+++
@@ -1,4 +0,0 @@
-# grafana-sunburst-panel
-
-The sunburst panel is a more powerful pie chart.
-It can contain nested group data.
